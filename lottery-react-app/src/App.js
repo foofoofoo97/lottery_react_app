3e6a8ba20f338fbf6bca357c93e43312cf25d176
foofoofoo <fooziqin@gmail.com>
2021-09-20 03:42:52 +0800

    first commit

---
+++ lottery-react-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./App.css";
+import lottery from "./lottery";
+import web3 from "./web3";
+
+class App extends Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}.<br />
+          There are currently {this.state.players.length} people enter competing
+          to win {web3.utils.fromWei(this.state.balance, "ether")} ether
+        </p>
+      </div>
+    );
+  }
+}
+export default App;
